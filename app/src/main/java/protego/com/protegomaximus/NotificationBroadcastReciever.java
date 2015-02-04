e913631dc6ac1ce4076a483ee2c56e541c4f8c7f
chanijindal <chaniajindal@gmail.com>
2015-02-04 14:18:45 +0530

    Notification

--- app/src/main/java/protego/com/protegomaximus/NotificationBroadcastReciever.java
+++ app/src/main/java/protego/com/protegomaximus/NotificationBroadcastReciever.java
@@ -4,13 +4,18 @@ import android.app.NotificationManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.support.v4.app.NotificationCompat;
 import android.widget.Toast;
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
+import android.annotation.TargetApi;
 
 /**
  * Created by chanijindal on 03/02/15.
  */
 public class NotificationBroadcastReciever extends BroadcastReceiver {
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
     @Override
 
 
@@ -24,6 +29,18 @@ public class NotificationBroadcastReciever extends BroadcastReceiver {
         mBuilder.setContentTitle("New Message");
         mBuilder.setContentText("Anomaly detected");
         mBuilder.setNumber(++notification_number);
+        Intent resultIntent = new Intent(context,MainActivity.class);
+
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(context);
+        stackBuilder.addParentStack(MainActivity.class);
+        stackBuilder.addNextIntent(resultIntent);
+        PendingIntent resultPendingIntent =
+                stackBuilder.getPendingIntent(
+                        0,
+                        PendingIntent.FLAG_UPDATE_CURRENT
+                );
+
+        mBuilder.setContentIntent(resultPendingIntent);
         NotificationManager mNotificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
 
 
